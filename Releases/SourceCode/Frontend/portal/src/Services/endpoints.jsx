ee21793ff9cfe53251cb7e71439e13534f235a5c
Wishva Perera <vibushitha99@gmail.com>
2021-10-05 00:59:37 +0530

    add route integrated

--- Releases/SourceCode/Frontend/portal/src/Services/endpoints.jsx
+++ Releases/SourceCode/Frontend/portal/src/Services/endpoints.jsx
@@ -4,3 +4,10 @@ export const baseURL = "http://localhost:9091/";
 //TIME_TABLE
 export const TIME_TABLE = baseURL + "timetable/";
 export const ADD_TIME_TABLE = TIME_TABLE + "addTimetable";
+
+// ROUTE
+export const ROUTE = baseURL + "routes/";
+export const ADD_MAIN_ROUTE = ROUTE + "addMainRoutes";
+export const ADD_SUB_ROUTE = ROUTE + "addRoute";
+export const GET_ALL_ROUTE = ROUTE + "getAllMainRoutes";
+export const GET_ROUTE_by_ID = ROUTE + "getMainRoutesById?routeId=";
