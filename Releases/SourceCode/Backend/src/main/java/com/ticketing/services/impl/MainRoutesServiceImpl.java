1fb5d1f367846373ad383819dc44d02a9eb99682
tlorence <tirone.lorence@gmail.com>
2021-10-05 00:00:36 +0530

    BE

---
+++ Releases/SourceCode/Backend/src/main/java/com/ticketing/services/impl/MainRoutesServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ticketing.services.impl;
+
+import com.ticketing.model.MainRoutes;
+import com.ticketing.repositories.MainRoutesRepository;
+import com.ticketing.services.MainRoutesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class MainRoutesServiceImpl implements MainRoutesService {
+
+    @Autowired
+    MainRoutesRepository mainRoutesRepository;
+
+    @Override
+    public MainRoutes addMainRoutes(MainRoutes mainRoutes) {
+        return mainRoutesRepository.save(mainRoutes);
+    }
+
+    @Override
+    public MainRoutes getMainRoute(String routeId) {
+        return mainRoutesRepository.getMainRoutesByRouteId(routeId);
+    }
+}
